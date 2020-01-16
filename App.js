8d5ba63cc98f8f65e85e8f5ca345051a1eedef12
raphyuki <raph.yuki@gmail.com>
2020-01-15 22:18:44 -0200

    added styling

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
-import { Image, Text, View } from 'react-native';
+import { StyleSheet, Image, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
 
 class Greeting extends Component {
   render() {
@@ -11,6 +22,31 @@ class Greeting extends Component {
   }
 }
 
+class Blink extends Component {
+
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+
+  //state object
+  state = { isShowingText: true };
+
+  render() {
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    return (
+      <Text>{this.props.text}</Text>
+    );
+  }
+}
+
 export default class Bananas extends Component {
   render() {
     let pic = {
@@ -18,9 +54,17 @@ export default class Bananas extends Component {
     };
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Bananas below</Text>
+        <View style={{width: 50, height: 30, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 30, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 30, backgroundColor: 'steelblue'}} />
+        <Text style={styles.red}>I'm red</Text>
+        <Text style={styles.bigBlue}>I'm blue!</Text>
+        <Blink text='Peek-a-boo!' />
         <Image source={pic} style={{width: 193, height: 110}}/>
         <Greeting name='Bananas' />
+        <View style={{width: 150, height: 30, backgroundColor: 'steelblue'}} />
+        <View style={{width: 100, height: 30, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 30, backgroundColor: 'powderblue'}} />
       </View>
     );
   }
